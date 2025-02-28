f7c10e80c813b53e4d7ff540a8569396f61779ca
phantom-merger <149815696+phantom-merger@users.noreply.github.com>
2025-02-28 16:16:41 +0000

    Add production deployment script for automated server updates

---
+++ build_prod.sh
@@ -0,0 +1,28 @@
+php artisan down &&
+echo "Application is now in maintenance mode" &&
+git pull origin master &&
+echo "Pulled latest changes from master" &&
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev -o &&
+echo "Composer install completed" &&
+# composer install --prefer-dist 
+# echo "Composer removed dev dependencies" &&
+php artisan migrate &&
+echo "Migrated successfully!" &&
+npm install &&
+echo "NPM install completed" &&
+sudo chown -R root:staff /var/www/html/kingbaker/production/ &&
+echo "Changed ownership to root:staff" &&
+npm run prod && 
+echo "Built successfully!" &&
+sudo chown -R www-data:www-data /var/www/html/kingbaker/production &&
+echo "Changed ownership to www-data:www-data" &&
+php artisan optimize &&
+echo "Application optimized" &&
+php artisan up &&
+echo "Application is now live" &&
+sudo service apache2 restart && 
+echo "Apache restarted" &&
+echo "Deployed successfully!" &&
+php artisan optimize &&
+echo "Application optimized"
+
